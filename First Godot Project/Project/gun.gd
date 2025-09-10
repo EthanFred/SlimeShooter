dcbdd68632b93542efadc486a5299054ac263a93
EthanFred <97852914+EthanFred@users.noreply.github.com>
2025-09-09 21:31:25 -0600

    Added files

---
+++ First Godot Project/Project/gun.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var canShoot = true
+func _physics_process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("shoot"):
+		if(canShoot):
+			shoot()
+			
+	#var enemies_in_range = get_overlapping_bodies()
+	#if enemies_in_range.size() > 0:
+		#var target_enemy = enemies_in_range[0]
+		#look_at(target_enemy.global_position)
+
+func shoot():
+	canShoot = false
+	%Reload.start(.15)
+	const BULLET = preload("res://Bullet.tscn")
+	
+	var newBullet = BULLET.instantiate()
+	newBullet.global_position = %ShootingPoint.global_position
+	newBullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(newBullet)
+
+
+func _on_reload_timeout() -> void:
+	canShoot = true # Replace with function body.
---
+++ First Godot Project/Project/mob.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+var health = 2
+var dir = 1
+var isKnockback = false
+var knockbackTime = .5
+var knockbackElapsed = 0.0
+
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(delta: float) -> void:
+	#If there is knockback
+	if(isKnockback):
+		#increment knockbackElpased by the time that has passed
+		knockbackElapsed += delta
+		
+		#Get the percentage of time elapsed
+		var t = clamp(knockbackElapsed / knockbackTime, 0, 1)
+		
+		dir = lerp(-1.0, 1.0, t)
+		
+		if(t >= 1):
+			isKnockback = false
+		
+	var direction = dir * global_position.direction_to(player.global_position)
+	velocity = direction * 300
+	move_and_slide()
+	
+func _ready(): 
+	%Slime.play_walk()
+	
+func take_damage():
+	health -= 1
+	%Slime.play_hurt()
+	
+	
+	if health == 0:
+		queue_free()
+		const SMOKESCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKESCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
+	else:
+		isKnockback = true
+		
+		
+		
+	
+		
